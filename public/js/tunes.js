d2e8dec2710f94653f41453be12c08827a329507
Justin Giancola <justin.giancola@gmail.com>
2013-02-11 16:51:41 -0500

    Add audio element and complete player controller actions

--- public/js/tunes.js
+++ public/js/tunes.js
@@ -73,6 +73,59 @@ Tunes.PlayerView = Em.View.extend({
   tagName: 'nav'
 });
 
+Tunes.PlayerController = Em.Controller.extend({
+  needs: ['playlist'],
+
+  currentTrack: null,
+  currentTrackBinding: 'controllers.playlist.currentTrack',
+
+  isPlaying: false,
+
+  init: function(){
+    this._super();
+
+    var audio = new Audio();
+
+    audio.addEventListener('ended', function() {
+      this.get('target').send('next');
+    }.bind(this));
+
+    this.set('audio', audio);
+  },
+
+  play: function() {
+    if (!this.get('currentTrack')) {
+      return;
+    }
+
+    // NOTE: queue playing the track once all of the current bindings
+    // sync to ensure currentTrack and audio src have been updated
+    Em.run.schedule('sync', this, function() {
+      this.get('audio').play();
+      this.set('isPlaying', true);
+    });
+  },
+
+  pause: function() {
+    this.get('audio').pause();
+    this.set('isPlaying', false);
+  },
+
+  currentTrackChanged: function() {
+    var new_src = this.get('currentTrack.url');
+
+    this.get('audio').src = new_src;
+
+    if (!new_src) {
+      this.pause();
+    }
+
+    if (this.get('isPlaying')) {
+      this.play();
+    }
+  }.observes('currentTrack')
+});
+
 Tunes.PlaylistController = Em.ArrayController.extend({
   currentTrack: null,
 
